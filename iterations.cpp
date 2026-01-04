500ca463203d135a4069b3e85eaf476cf0653b93
nithyanandhar <nithyanandharmani@gmail.com>
2026-01-04 19:49:11 +0530

    itertaions and math function

---
+++ iterations.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*
+    //while loop
+    int m=1;
+    while (m<=10){
+        cout<<m<<endl;
+        m++;
+    }
+    m=1;
+    cout<<endl;
+    //do while loop
+    do {
+        cout<<m<<endl;
+        m++;
+    }while(m>=2 && m<=10);
+    //for loop
+    int n=5;
+    for(int i=0;i<=n;i++){
+        cout<<i<<endl;
+    }
+    */
+    int k=0;
+    while(k>=0){
+        cout<<k<<endl;
+    }
+
+
+    return 0 ;
+}
