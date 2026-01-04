0abd17d2891f7b858e71c75efe5ae8597cef05f7
nithyanandhar <nithyanandharmani@gmail.com>
2026-01-04 19:54:23 +0530

    iterations and math functions

--- iterations.cpp
+++ iterations.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
     /*
@@ -20,12 +21,20 @@ int main(){
     for(int i=0;i<=n;i++){
         cout<<i<<endl;
     }
-    */
     int k=0;
     while(k>=0){
         cout<<k<<endl;
     }
+    */
 
+    int n1=2,n2=3;
+    float n3=2.5;
+    cout<<max(n1,n2)<<endl;
+    cout<<min(n1,n2)<<endl;
+    cout<<pow(n1,n2)<<endl;
+    cout<<round(n3)<<endl;
+    cout<<log(2)<<endl;
+    cout<<sqrt(2)<<endl;
 
     return 0 ;
 }
